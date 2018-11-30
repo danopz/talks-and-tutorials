7c2af30ca749fe5aed64decef9c3a1529e30308e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-30 22:31:20 +0000

    Deploy danopz/talks-and-tutorials to github.com/danopz/talks-and-tutorials.git:gh-pages

---
+++ precache-manifest.f743472eb4231b2f443c8ad9cd33ef15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "996758f47ee9d1501d86",
+    "url": "/talks-and-tutorials/static/js/runtime~main.996758f4.js"
+  },
+  {
+    "revision": "6314962260874445ff6f",
+    "url": "/talks-and-tutorials/static/js/main.63149622.chunk.js"
+  },
+  {
+    "revision": "e9d5f5c4131fae561068",
+    "url": "/talks-and-tutorials/static/js/1.e9d5f5c4.chunk.js"
+  },
+  {
+    "revision": "6314962260874445ff6f",
+    "url": "/talks-and-tutorials/static/css/main.6ea8a083.chunk.css"
+  },
+  {
+    "revision": "e9d5f5c4131fae561068",
+    "url": "/talks-and-tutorials/static/css/1.a68cec28.chunk.css"
+  },
+  {
+    "revision": "86a65f8f2f1f360e60fa3a28d2edbbcc",
+    "url": "/talks-and-tutorials/index.html"
+  }
+];
